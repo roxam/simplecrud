b634031450c99167402691c970ecab20e8d98a95
Roxane <roxanew@gmail.com>
2016-02-04 15:31:55 -0800

    convert list page to Angular

--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -1,16 +1,27 @@
-$(document).ready(function () {
 
-    $.getJSON('/articles', printTerms);
-    
+(function(){
 
-});
+    var app = angular.module('articleApp',[]);
 
-function printTerms(terms) {
-    $('article').empty();
+    app.controller('ArticleController',['$scope','$http','$sce',function($scope, $http, $sce){
 
-    $.each(terms, function () {
-        $('<h2>').text(this.title).appendTo('article');
-        $('<p>').text(this.content).appendTo('article');
-    });
-    
-}
+
+        $http.get('/articles').then(function(printTerms){
+            $scope.articleData = printTerms.data;
+
+
+        });
+
+        /*$scope.printTerms = function (terms) {
+            $('article').empty();
+
+            $.each(terms, function () {
+                $('<h2>').text(this.title).appendTo('article');
+                $('<p>').text(this.content).appendTo('article');
+            });
+            
+        };*/
+
+    }]);
+
+})();
